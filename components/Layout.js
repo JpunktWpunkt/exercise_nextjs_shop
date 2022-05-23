03f12b5b90fc4424dd7dbd4e632e00970ed5e7a1
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-05-23 17:46:54 +0200

    add Product Page

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from "@mui/material/Container";
 import Header from '../components/Header';
 import Footer from "./Footer";
+import Head from "next/head";
 
 const style = {
     display: "flex",
@@ -14,9 +15,12 @@ const style = {
 };
 
 
-const Layout = ({children}) => {
+const Layout = ({title, children}) => {
     return (
         <div>
+            <Head>
+                <title>{title ? `${title}- Next Amazona` : 'Next Amazona'}</title>
+            </Head>
             <Header/>
             <Container sx={{...style}}>
                 {children}
